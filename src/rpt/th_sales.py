3937ff8d8e85d018cdb8eeba871e3f706da78246
EJ <pej111797@gmail.com>
2022-01-13 18:50:26 +0900

    #04_EJ_EDA_RE

--- src/rpt/th_sales.py
+++ src/rpt/th_sales.py
@@ -5,48 +5,43 @@ import matplotlib.gridspec as gridspec
 from matplotlib import font_manager, rc
 import matplotlib
 import seaborn as sns
+import pandas as pd
 
 # Setup
-font_name = font_manager.FontProperties(fname="src/gulim.ttc").get_name()
+font_name = font_manager.FontProperties(fname="src/BMDOHYEON_ttf.ttf").get_name()
 plt.rc('font', family=font_name)
 matplotlib.rcParams['axes.unicode_minus'] = False
 
 """
 빅데이터_상권_분석 : 상권_추정매출
-분석 결과 1 : 
+분석 결과 1 : 분기별 매출은 코로나시기에 줄어들었으나, 2019년과 비교하여서 상대적으로 높은 매출의 그래프. 남성 & 여성 매출건수는 분기별 매출과 매우 비슷한 그래프 형태를 띄고 있음.
+
+그래프 2 [분기별 매출금액(10억)과 점포수] : 코로나시기에 줄어든 매출금액과 반대로 점포수는 반대로 증가하였음을 보여준다.
+그래프 3 [2020-2019년 매출차액 상,하위 10개 서비스업종별 매출금액] : 
+의료품의 상승세와 가장많은 점포수를 가지고 있는 한식점의 하락은 코로나 시기에도 불구하고 2020년의 매출이 2019년보다 높은 이유를 설명해준다.
 """
 
 # 001 : Data Load
-상권_추정매출 = setup_tradehub('상권_추정매출.csv')
+상권_추정매출_2016 = setup_tradehub('상권_추정매출_2016.csv')
+상권_추정매출_2017 = setup_tradehub('상권_추정매출_2017.csv')
+상권_추정매출_2018 = setup_tradehub('상권_추정매출_2018.csv')
+상권_추정매출_2019 = setup_tradehub('상권_추정매출_2019.csv')
+상권_추정매출_2020 = setup_tradehub('상권_추정매출_2020.csv')
+상권_추정매출_2021 = setup_tradehub('상권_추정매출_2021.csv')
 
-# EDA PART 7 : 추정매출
-'''
-질문 1 : 분기별 매출은 줄어들었을까 ? 남성매출건수, 여성매출건수로 보면은 차이가 있을까?
-그래프 1 [분기당 매출금액(10억)과 남성&여성 매출건수(1만)] : 분기별 매출은 코로나시기에 줄어들었으나, 2019년과 비교하여서
-상대적으로 높은 매출의 그래프. 남성 & 여성 매출건수는 분기별 매출과 매우 비슷한 그래프 형태를 띄고 있음.
+상권_추정매출 = pd.concat([상권_추정매출_2016, 상권_추정매출_2017, 상권_추정매출_2018, 상권_추정매출_2019, 상권_추정매출_2020, 상권_추정매출_2021]).reset_index(drop=True)
 
-질문 2 : 분기별 매출금액과 점포수는 동일하게 움직일까 ?
-그래프 2 [분기별 매출금액(10억)과 점포수] : 코로나시기에 줄어든 매출금액과 반대로 점포수는 반대로 증가하였음을 보여준다.
+# 002 : [EDA : LINE] Total Quarter View Plot
+분기별_추정매출 = 상권_추정매출.groupby(['기준_년_코드', '기준_분기_코드', '분기_코드']).agg(
+    분기당_매출_금액=('분기당_매출_금액', 'sum'), 분기당_매출_건수=('분기당_매출_건수', 'sum'),
+    주중_매출_비율=('주중_매출_비율','mean'), 주말_매출_비율=('주말_매출_비율','mean'),
+    점포수=('점포수', 'sum'), 남성_매출_건수=('남성_매출_건수', 'sum'), 여성_매출_건수=('여성_매출_건수', 'sum')).reset_index()
 
-질문 3 : 상하위 서비스업종별 매출의 분기별 차이는 ? 2019년 매출이 2020년 매출보다 높은이유를 설명하기 위한 차트로 충분한가?
-그래프 3 [2020-2019년 매출차액 상,하위 10개 서비스업종별 매출금액] : 의료품의 상승세와 가장많은 점포수를 가지고 있는 한식점의 하락은
-코로나시기에도 불구하고 2020년의 매출이 2019년보다 높은 이유를 설명해준다.
-'''
-추정매출['분기_코드'] = 추정매출['기준_년_코드'].astype(str) + 'Q' + 추정매출['기준_분기_코드'].astype(str)
-eda_분기별_추정매출 = 추정매출.groupby(['기준_년_코드', '기준_분기_코드', '분기_코드']).agg(
-    분기당_매출_금액=('분기당_매출_금액', 'sum'),
-    분기당_매출_건수=('분기당_매출_건수', 'sum'),
-    점포수=('점포수', 'sum'),
-    남성_매출_건수=('남성_매출_건수', 'sum'),
-    여성_매출_건수=('여성_매출_건수', 'sum'),
-).reset_index()
-
-# 그래프 1 : 분기당 매출금액(10억)과 남성&여성 매출건수(1만)
 fig, ax = plt.subplots()
-ax.plot(eda_분기별_추정매출['분기_코드'], eda_분기별_추정매출['분기당_매출_금액'] / 1000000000, label='분기당_매출_금액(10억)')
-ax.plot(eda_분기별_추정매출['분기_코드'], eda_분기별_추정매출['남성_매출_건수'] / 10000, label='남성_매출_건수(1만)')
-ax.plot(eda_분기별_추정매출['분기_코드'], eda_분기별_추정매출['여성_매출_건수'] / 10000, label='여성_매출_건수(1만)')
-ax.set_title("분기당 매출금액(10억)과 남성&여성 매출건수(1만)", fontsize=20)
+ax.plot(분기별_추정매출['분기_코드'], 분기별_추정매출['분기당_매출_금액'] / 1000000000, label='분기당_매출_금액(10억)')
+ax.plot(분기별_추정매출['분기_코드'], 분기별_추정매출['남성_매출_건수'] / 10000, label='남성_매출_건수(1만)')
+ax.plot(분기별_추정매출['분기_코드'], 분기별_추정매출['여성_매출_건수'] / 10000, label='여성_매출_건수(1만)')
+ax.set_title("분기당 매출 금액(10억)과 남성&여성 매출 건수(1만)", fontsize=20)
 ax.set_xlabel('분기')
 ax.set_ylabel('매출금액(10억),매출건수(1만)')
 ax.grid(linewidth=0.3)
@@ -56,26 +51,127 @@ plt.xticks(rotation=45)
 plt.legend(loc="upper right")
 plt.show()
 
-# 그래프 2 : 분기당 매출금액(10억)과 점포수
+# 분기당 매출금액(10억)과 점포수
 fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.plot(eda_분기별_추정매출['분기_코드'], eda_분기별_추정매출['분기당_매출_금액'] / 1000000000, label='분기당_매출_금액(10억)')
+ax1.plot(분기별_추정매출['분기_코드'], 분기별_추정매출['분기당_매출_금액'] / 1000000000, label='분기당_매출_금액(10억)')
 ax1.set_title("분기별 매출금액(10억)", fontsize=15)
 ax1.set_ylabel('매출금액(10억)')
 ax1.grid(linewidth=0.3)
 ax1.axvline(x='2020Q1', color='darkred', linestyle='--')
-ax2.plot(eda_분기별_추정매출['분기_코드'], eda_분기별_추정매출['점포수'], label='점포수', color='orange')
+ax2.plot(분기별_추정매출['분기_코드'], 분기별_추정매출['점포수'], label='점포수', color='orange')
 ax2.set_title("분기별 점포수", fontsize=18)
 ax2.set_ylabel('점포수')
 ax2.grid(linewidth=0.3)
 ax2.axvline(x='2020Q1', color='darkred', linestyle='--')
+
 plt.setp(ax1.get_xticklabels(), rotation=45)
 plt.setp(ax2.get_xticklabels(), rotation=45)
 fig.subplots_adjust(wspace=0.2, hspace=0.5)
 fig.suptitle('분기별 매출금액(10억)과 점포수', fontsize=20)
 plt.show()
 
-# 그래프 3 : 2020-2019년 매출차액 상,하위 10개 서비스업종별 매출금액
-eda_추정매출 = pd.pivot_table(추정매출, values=['분기당_매출_금액'], index=['서비스_업종_코드_명'], columns=['기준_년_코드'],
+# 003 : [EDA : LINE] Quarter View Plot, Category by Region
+시군구_추정매출 = 상권_추정매출.groupby(['시군구','분기_코드']).agg(
+    분기당_매출_금액=('분기당_매출_금액', 'sum'), 분기당_매출_건수=('분기당_매출_건수', 'sum'),
+    점포수=('점포수', 'sum'), 남성_매출_건수=('남성_매출_건수', 'sum'), 여성_매출_건수=('여성_매출_건수', 'sum')).reset_index()
+
+# 총 매출 금액 시각화
+ax = sns.lineplot(x='분기_코드',
+             y='분기당_매출_금액',
+             hue='시군구',
+             data=시군구_추정매출)
+
+plt.title('구별 분기당 매출 금액', fontsize=20)
+plt.ylabel('총 매출 금액', fontsize=14)
+plt.xlabel('분기', fontsize=14)
+plt.legend(fontsize=12, loc='best')
+plt.show();
+
+# 총 매출 건수 시각화
+ax = sns.lineplot(x='분기_코드',
+             y='분기당_매출_건수',
+             hue='시군구',
+             data=시군구_추정매출)
+
+plt.title('구별 분기당 매출 건수', fontsize=20)
+plt.ylabel('총 매출 건수', fontsize=14)
+plt.xlabel('분기', fontsize=14)
+plt.legend(fontsize=12, loc='best')
+plt.show();
+
+# 총 점포수 시각화
+ax = sns.lineplot(x='분기_코드',
+             y='점포수',
+             hue='시군구',
+             data=시군구_추정매출)
+
+plt.title('구별 점포수', fontsize=20)
+plt.ylabel('총 점포수', fontsize=14)
+plt.xlabel('분기', fontsize=14)
+plt.legend(fontsize=12, loc='best')
+plt.show();
+
+# 004 : [EDA : BAR] Category by Region
+시군구_추정매출2 = 시군구_추정매출.groupby(['시군구']).분기당_매출_금액.mean().reset_index()
+시군구_추정매출2 = 시군구_추정매출2.sort_values(by=['분기당_매출_금액'], ascending=True)
+시군구_추정매출2 = 시군구_추정매출2.set_index('시군구')
+
+# 총 매출 금액 시각화
+my_colors = ['#fcd5ce', '#e8e8e4', '#ffd7ba']
+plt.barh(시군구_추정매출2.index, 시군구_추정매출2['분기당_매출_금액'], color=my_colors)
+plt.title('구별 총 매출 금액', fontsize=18)
+plt.ylabel('시군구', fontsize=15)
+plt.xlabel('총 매출 금액', fontsize=15)
+plt.show();
+
+시군구_추정매출2 = 시군구_추정매출.groupby(['시군구']).분기당_매출_건수.mean().reset_index()
+시군구_추정매출2 = 시군구_추정매출2.sort_values(by=['분기당_매출_건수'], ascending=True)
+시군구_추정매출2 = 시군구_추정매출2.set_index('시군구')
+
+# 총 매출 건수 시각화
+my_colors = ['#fcd5ce', '#e8e8e4', '#ffd7ba']
+plt.barh(시군구_추정매출2.index, 시군구_추정매출2['분기당_매출_건수'], color=my_colors)
+plt.title('구별 총 매출 건수', fontsize=18)
+plt.ylabel('시군구', fontsize=15)
+plt.xlabel('총 매출 건수', fontsize=15)
+plt.show();
+
+시군구_추정매출2 = 시군구_추정매출.groupby(['시군구']).점포수.mean().reset_index()
+시군구_추정매출2 = 시군구_추정매출2.sort_values(by=['점포수'], ascending=True)
+시군구_추정매출2 = 시군구_추정매출2.set_index('시군구')
+
+# 총 점포수 시각화
+my_colors = ['#fcd5ce', '#e8e8e4', '#ffd7ba']
+plt.barh(시군구_추정매출2.index, 시군구_추정매출2['점포수'], color=my_colors)
+plt.title('구별 총 점포수', fontsize=18)
+plt.ylabel('시군구', fontsize=15)
+plt.xlabel('총 점포수', fontsize=15)
+plt.show();
+
+# 여기서부터는 추가 수정할 예정
+# 005 : [EDA : BAR] Category by 상권_코드_명
+상권코드_추정매출 = 상권_추정매출.groupby(['상권_코드_명']).agg(
+    분기당_매출_금액=('분기당_매출_금액', 'mean'), 분기당_매출_건수=('분기당_매출_건수', 'mean'),
+    점포수=('점포수', 'mean'), 남성_매출_건수=('남성_매출_건수', 'mean'), 여성_매출_건수=('여성_매출_건수', 'mean')).reset_index()
+
+# 상위 10 / 하위 10 매출 금액
+top_상권코드_추정매출 = 상권코드_추정매출.sort_values(by=['분기당_매출_금액'], ascending=False).head(10)
+btm_상권코드_추정매출 = 상권코드_추정매출.sort_values(by=['분기당_매출_금액'], ascending=False).tail(10)
+
+# 006 : [EDA : BAR] Category by 서비스_업종_코드_명
+서비스업종코드_추정매출 = 상권_추정매출.groupby(['서비스_업종_코드_명']).agg(
+    분기당_매출_금액=('분기당_매출_금액', 'mean'), 분기당_매출_건수=('분기당_매출_건수', 'mean'),
+    점포수=('점포수', 'mean'), 남성_매출_건수=('남성_매출_건수', 'mean'), 여성_매출_건수=('여성_매출_건수', 'mean')).reset_index()
+
+서비스업종코드_상권코드_추정매출 = 상권_추정매출.groupby(['상권_코드_명','서비스_업종_코드_명']).agg(
+    분기당_매출_금액=('분기당_매출_금액', 'mean'), 분기당_매출_건수=('분기당_매출_건수', 'mean'),
+    점포수=('점포수', 'mean'), 남성_매출_건수=('남성_매출_건수', 'mean'), 여성_매출_건수=('여성_매출_건수', 'mean')).reset_index()
+
+
+
+
+# 2020-2019년 매출차액 상,하위 10개 서비스업종별 매출금액
+eda_추정매출 = pd.pivot_table(상권_추정매출, values=['분기당_매출_금액'], index=['서비스_업종_코드_명'], columns=['기준_년_코드'],
                           aggfunc=sum).reset_index()
 eda_추정매출 = eda_추정매출.droplevel(0, axis=1).reset_index().rename_axis(columns=None)
 eda_추정매출 = eda_추정매출.rename(columns={'': '서비스_업종_코드_명'})
@@ -83,24 +179,22 @@ eda_추정매출['차액'] = eda_추정매출[2020] - eda_추정매출[2019]
 edatop_추정매출 = eda_추정매출.sort_values(by='차액', ascending=False).head(10)
 edabtm_추정매출 = eda_추정매출.sort_values(by='차액', ascending=False).tail(10)
 
-edatop_상권별_추정매출 = 추정매출[추정매출['서비스_업종_코드_명'].isin(edatop_추정매출['서비스_업종_코드_명'])]
-edabtm_상권별_상주인구 = 추정매출[추정매출['서비스_업종_코드_명'].isin(edabtm_추정매출['서비스_업종_코드_명'])]
+edatop_상권별_추정매출 = 상권_추정매출[상권_추정매출['서비스_업종_코드_명'].isin(edatop_추정매출['서비스_업종_코드_명'])]
+edabtm_상권별_상주인구 = 상권_추정매출[상권_추정매출['서비스_업종_코드_명'].isin(edabtm_추정매출['서비스_업종_코드_명'])]
 
 edatop_상권별_추정매출 = edatop_상권별_추정매출.groupby(['기준_년_코드', '기준_분기_코드', '분기_코드', '서비스_업종_코드_명']).agg(
     분기당_매출_금액=('분기당_매출_금액', 'sum'),
     분기당_매출_건수=('분기당_매출_건수', 'sum'),
     점포수=('점포수', 'sum'),
     남성_매출_건수=('남성_매출_건수', 'sum'),
-    여성_매출_건수=('여성_매출_건수', 'sum'),
-).reset_index()
+    여성_매출_건수=('여성_매출_건수', 'sum')).reset_index()
 
 edabtm_상권별_추정매출 = edabtm_상권별_상주인구.groupby(['기준_년_코드', '기준_분기_코드', '분기_코드', '서비스_업종_코드_명']).agg(
     분기당_매출_금액=('분기당_매출_금액', 'sum'),
     분기당_매출_건수=('분기당_매출_건수', 'sum'),
     점포수=('점포수', 'sum'),
     남성_매출_건수=('남성_매출_건수', 'sum'),
-    여성_매출_건수=('여성_매출_건수', 'sum'),
-).reset_index()
+    여성_매출_건수=('여성_매출_건수', 'sum')).reset_index()
 
 edatop_상권별_추정매출['분기당_매출_금액'] = edatop_상권별_추정매출['분기당_매출_금액'] / 1000000000
 edabtm_상권별_추정매출['분기당_매출_금액'] = edabtm_상권별_추정매출['분기당_매출_금액'] / 1000000000
--- src/rpt/th_store.py
+++ src/rpt/th_store.py
@@ -5,29 +5,22 @@ import matplotlib.gridspec as gridspec
 from matplotlib import font_manager, rc
 import matplotlib
 import seaborn as sns
+import pandas as pd
 
 # Setup
-font_name = font_manager.FontProperties(fname="src/gulim.ttc").get_name()
+font_name = font_manager.FontProperties(fname="src/BMDOHYEON_ttf.ttf").get_name()
 plt.rc('font', family=font_name)
 matplotlib.rcParams['axes.unicode_minus'] = False
 
 """
 빅데이터_상권_분석 : 상권_점포
-분석 결과 1 : 
-"""
+분석 결과 1 : 2019Q1부터 데이터 존재. 중요한 데이터. 하지만 예)한식음식점이 증가하는 것이 유동인구 혹은 골목상권에 어떠한 영향이 있을까?
 
-# 001 : Data Load
-상권_점포 = setup_tradehub('상권_점포.csv')
-
-# EDA PART 4 : 점포
-'''
-***** 2019Q1부터 데이터 존재.
-***** 중요한 데이터. 하지만 예)한식음식점이 증가하는것이 유동인구 혹은 골목상권에 어떠한 영향이 있을까?
 질문 1 : 분기별 점포수는 코로나 이후 얼마 줄었을까? 폐업과 같이 확인
 그래프 1 [2019-2021 분기별 점포수] : 코로나시기에 증가하는 점포수. 하지만 2020Q1이후로 줄어드는 트렌드.
 
 질문 2 : 상위 15 점포의 변화는 어떨까 ?
-그래프 2 & 3 [상위15 서비스업종별 점포 & 프랜차이즈 수] [서비스업종별 상위15 점포 & 프랜차이즈 수] :
+[상위15 서비스업종별 점포 & 프랜차이즈 수] [서비스업종별 상위15 점포 & 프랜차이즈 수] :
 첫벗째, 점포수 & 프랜차이즈 점포수는 크게 눈에띄게 하락하지 않았다.
 두번째, 눈에 띄지않지만 줄어드는 개업점포수와 2020Q2 이후 증가하는 폐업수. 코로나의 영향이라 할수 있을만함 움직임.
 세번째, 코로나 기간인 2020Q1 상위 15 서비스업종에서 보이는 폐업수
@@ -37,42 +30,45 @@ matplotlib.rcParams['axes.unicode_minus'] = False
 -전자상거래업 12퍼센트 상승
 -일반교습학원 10퍼센트상승
 -호프간이주점 7퍼센트 상승
--분식전문저 5퍼센트 하락
-'''
-점포['분기_코드'] = 점포['기준_년_코드'].astype(str) + 'Q' + 점포['기준_분기_코드'].astype(str)
+-분식전문점 5퍼센트 하락
+"""
 
-# 분기별 점포수
-eda_분기별_점포 = 점포.groupby(['기준_년_코드','기준_분기_코드','분기_코드']).agg(
-    점포_수=('점포_수', 'sum'),
-    개업_점포_수=('개업_점포_수', 'sum'),
-    폐업_점포_수=('폐업_점포_수', 'sum'),
-    프랜차이즈_점포_수=('프랜차이즈_점포_수', 'sum')
-).reset_index()
+# 001 : Data Load
+상권_점포_2016 = setup_tradehub('상권_점포_2016.csv')
+상권_점포_2017 = setup_tradehub('상권_점포_2017.csv')
+상권_점포_2018 = setup_tradehub('상권_점포_2018.csv')
+상권_점포_2019 = setup_tradehub('상권_점포_2019.csv')
+상권_점포_2020_2021 = setup_tradehub('상권_점포_2020_2021.csv')
 
-# 그래프 1
+상권_점포 = pd.concat([상권_점포_2016, 상권_점포_2017, 상권_점포_2018, 상권_점포_2019, 상권_점포_2020_2021]).reset_index(drop=True)
+
+# 002 : [EDA : LINE] Total Quarter View Plot
+분기별_점포 = 상권_점포.groupby(['기준_년_코드','기준_분기_코드','분기_코드']).agg(
+    점포_수=('점포_수', 'sum'), 개업_점포_수=('개업_점포_수', 'sum'), 폐업_점포_수=('폐업_점포_수', 'sum'),프랜차이즈_점포_수=('프랜차이즈_점포_수', 'sum')).reset_index()
+
 fig = plt.figure()
 gs = gridspec.GridSpec(2, 2)
 ax1 = fig.add_subplot(gs[0, 0])
 ax2 = fig.add_subplot(gs[0, 1])
 ax3 = fig.add_subplot(gs[1, :])
 
-ax1.plot(eda_분기별_점포['분기_코드'], eda_분기별_점포['점포_수'], label='점포_수')
+ax1.plot(분기별_점포['분기_코드'], 분기별_점포['점포_수'], label='점포_수')
 ax1.set_title("분기별 점포 수", fontsize=12)
 ax1.set_xlabel('분기')
 ax1.set_ylabel('점포수')
 ax1.grid(which='major', linestyle='--', linewidth=0.5)
 ax1.grid(which='minor', linestyle=':', linewidth=0.5)
 
-ax2.plot(eda_분기별_점포['분기_코드'], eda_분기별_점포['프랜차이즈_점포_수'], label='프랜차이즈_점포_수')
-ax2.set_title("분기별 프랜차이즈_점포 수", fontsize=12)
+ax2.plot(분기별_점포['분기_코드'], 분기별_점포['프랜차이즈_점포_수'], label='프랜차이즈_점포_수')
+ax2.set_title("분기별 프랜차이즈 점포 수", fontsize=12)
 ax2.set_xlabel('분기')
 ax2.set_ylabel('점포수')
 ax2.grid(which='major', linestyle='--', linewidth=0.5)
 ax2.grid(which='minor', linestyle=':', linewidth=0.5)
 
-ax3.plot(eda_분기별_점포['분기_코드'], eda_분기별_점포['개업_점포_수'], label='개업_점포_수', color='teal')
-ax3.plot(eda_분기별_점포['분기_코드'], eda_분기별_점포['폐업_점포_수'], label='폐업_점포_수', color='orange')
-ax3.set_title("분기별 개업&폐업 수", fontsize=12)
+ax3.plot(분기별_점포['분기_코드'], 분기별_점포['개업_점포_수'], label='개업_점포_수', color='teal')
+ax3.plot(분기별_점포['분기_코드'], 분기별_점포['폐업_점포_수'], label='폐업_점포_수', color='orange')
+ax3.set_title("분기별 개업 수 & 폐업 수", fontsize=12)
 ax3.set_xlabel('분기')
 ax3.set_ylabel('점포수')
 ax3.grid(which='major', linestyle='--', linewidth=0.5)
@@ -81,23 +77,127 @@ ax3.grid(which='minor', linestyle=':', linewidth=0.5)
 plt.setp(ax1.get_xticklabels(), rotation=45)
 plt.setp(ax2.get_xticklabels(), rotation=45)
 fig.subplots_adjust(wspace=0.1, hspace=0.4)
-fig.suptitle('2019-2021 분기별 점포수', fontsize=20)
+fig.suptitle('분기별 점포수', fontsize=20)
 ax3.legend()
 plt.show()
 
-# 업종별 점포수
-eda_업종별_점포 = 점포.groupby(['기준_년_코드','기준_분기_코드','분기_코드','서비스_업종_코드_명']).agg(
+# 003 : [EDA : LINE] Quarter View Plot, Category by Region
+시군구_점포 = 상권_점포.groupby(['시군구','분기_코드']).agg(
+    점포_수=('점포_수', 'sum'), 개업_점포_수=('개업_점포_수', 'sum'), 폐업_점포_수=('폐업_점포_수', 'sum'),프랜차이즈_점포_수=('프랜차이즈_점포_수', 'sum')).reset_index()
+
+# 총 점포 수 시각화
+ax = sns.lineplot(x='분기_코드',
+             y='점포_수',
+             hue='시군구',
+             data=시군구_점포)
+
+plt.title('구별 총 점포 수', fontsize=20)
+plt.ylabel('총 점포 수', fontsize=14)
+plt.xlabel('분기', fontsize=14)
+plt.legend(fontsize=12, loc='best')
+plt.show();
+
+# 총 개업 점포 수 시각화
+ax = sns.lineplot(x='분기_코드',
+             y='개업_점포_수',
+             hue='시군구',
+             data=시군구_점포)
+
+plt.title('구별 총 개업 점포 수', fontsize=20)
+plt.ylabel('총 개업 점포 수', fontsize=14)
+plt.xlabel('분기', fontsize=14)
+plt.legend(fontsize=12, loc='best')
+plt.show();
+
+# 총 폐업 점포 수 시각화
+ax = sns.lineplot(x='분기_코드',
+             y='폐업_점포_수',
+             hue='시군구',
+             data=시군구_점포)
+
+plt.title('구별 총 폐업 점포 수', fontsize=20)
+plt.ylabel('총 폐업 점포 수', fontsize=14)
+plt.xlabel('분기', fontsize=14)
+plt.legend(fontsize=12, loc='best')
+plt.show();
+
+# 총 프랜차이즈 점포 수 시각화
+ax = sns.lineplot(x='분기_코드',
+             y='프랜차이즈_점포_수',
+             hue='시군구',
+             data=시군구_점포)
+
+plt.title('구별 총 프랜차이즈 점포 수', fontsize=20)
+plt.ylabel('총 프랜차이즈 점포 수', fontsize=14)
+plt.xlabel('분기', fontsize=14)
+plt.legend(fontsize=12, loc='best')
+plt.show();
+
+# 004 : [EDA : BAR] Category by Region
+시군구_점포2 = 시군구_점포.groupby(['시군구']).점포_수.mean().reset_index()
+시군구_점포2 = 시군구_점포2.sort_values(by=['점포_수'], ascending=True)
+시군구_점포2 = 시군구_점포2.set_index('시군구')
+
+# 총 점포 수 시각화
+my_colors = ['#fcd5ce', '#e8e8e4', '#ffd7ba']
+plt.barh(시군구_점포2.index, 시군구_점포2['점포_수'], color=my_colors)
+plt.title('구별 총 점포 수', fontsize=18)
+plt.ylabel('시군구', fontsize=15)
+plt.xlabel('총 점포 수', fontsize=15)
+plt.show();
+
+시군구_점포2 = 시군구_점포.groupby(['시군구']).개업_점포_수.mean().reset_index()
+시군구_점포2 = 시군구_점포2.sort_values(by=['개업_점포_수'], ascending=True)
+시군구_점포2 = 시군구_점포2.set_index('시군구')
+
+# 총 개업 점포 수 시각화
+my_colors = ['#fcd5ce', '#e8e8e4', '#ffd7ba']
+plt.barh(시군구_점포2.index, 시군구_점포2['개업_점포_수'], color=my_colors)
+plt.title('구별 총 개업 점포 수', fontsize=18)
+plt.ylabel('시군구', fontsize=15)
+plt.xlabel('총 개업 점포 수', fontsize=15)
+plt.show();
+
+시군구_점포2 = 시군구_점포.groupby(['시군구']).폐업_점포_수.mean().reset_index()
+시군구_점포2 = 시군구_점포2.sort_values(by=['폐업_점포_수'], ascending=True)
+시군구_점포2 = 시군구_점포2.set_index('시군구')
+
+# 총 폐업 점포 수 시각화
+my_colors = ['#fcd5ce', '#e8e8e4', '#ffd7ba']
+plt.barh(시군구_점포2.index, 시군구_점포2['폐업_점포_수'], color=my_colors)
+plt.title('구별 총 폐업 점포 수', fontsize=18)
+plt.ylabel('시군구', fontsize=15)
+plt.xlabel('총 폐업 점포 수', fontsize=15)
+plt.show();
+
+시군구_점포2 = 시군구_점포.groupby(['시군구']).프랜차이즈_점포_수.mean().reset_index()
+시군구_점포2 = 시군구_점포2.sort_values(by=['프랜차이즈_점포_수'], ascending=True)
+시군구_점포2 = 시군구_점포2.set_index('시군구')
+
+# 총 프랜차이즈 점포 수 시각화
+my_colors = ['#fcd5ce', '#e8e8e4', '#ffd7ba']
+plt.barh(시군구_점포2.index, 시군구_점포2['프랜차이즈_점포_수'], color=my_colors)
+plt.title('구별 총 프랜차이즈 점포 수', fontsize=18)
+plt.ylabel('시군구', fontsize=15)
+plt.xlabel('총 프랜차이즈 점포 수', fontsize=15)
+plt.show();
+
+# 005 : [EDA : BAR] Category by 상권_코드_명
+
+# 006 : [EDA : BAR] Category by 업종
+
+업종별_점포 = 상권_점포.groupby(['기준_년_코드','기준_분기_코드','분기_코드','서비스_업종_코드_명']).agg(
     점포_수=('점포_수', 'sum'),
     개업_점포_수=('개업_점포_수', 'sum'),
     폐업_점포_수=('폐업_점포_수', 'sum'),
     프랜차이즈_점포_수=('프랜차이즈_점포_수', 'sum')
 ).reset_index()
 
-eda_점포수 = pd.pivot_table(eda_업종별_점포, values = ['점포_수'], index=['서비스_업종_코드_명']).reset_index()
+eda_점포수 = pd.pivot_table(업종별_점포, values = ['점포_수'], index=['서비스_업종_코드_명']).reset_index()
 eda_점포수 = eda_점포수.sort_values(by='점포_수', ascending=False).head(15)
 eda_점포수 = eda_점포수['서비스_업종_코드_명']
 
-eda_업종별_점포 = pd.pivot_table(eda_업종별_점포, values = ['점포_수','개업_점포_수','폐업_점포_수','프랜차이즈_점포_수'], index=['서비스_업종_코드_명', '분기_코드']).reset_index()
+eda_업종별_점포 = pd.pivot_table(업종별_점포, values = ['점포_수','개업_점포_수','폐업_점포_수','프랜차이즈_점포_수'], index=['서비스_업종_코드_명', '분기_코드']).reset_index()
 eda_업종별_점포 = eda_업종별_점포[eda_업종별_점포['서비스_업종_코드_명'].isin(eda_점포수)]
 eda_업종별_점포['점포_수_pchange'] = eda_업종별_점포.groupby('서비스_업종_코드_명', sort=False)['점포_수'].apply(lambda x: x.pct_change()).to_numpy()
 eda_업종별_점포['개업_점포_수_pchange'] = eda_업종별_점포.groupby('서비스_업종_코드_명', sort=False)['개업_점포_수'].apply(lambda x: x.pct_change()).to_numpy()
